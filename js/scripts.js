3d28581c11f495da61eedb9c4094e475a245f714
gaetanovenuto <gaetanovenuto99@live.it>
2024-07-11 15:27:44 +0200

    completata struttura

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function calculate() {
+    const KmInput = document.getElementById("KmInput");
+    ;
+    const KmFixedPrice = 0.21;
+}
+
+console.log("Hello World!")
+
+// INPUT AGE 
+const AgeInput = document.getElementBy("AgeInput");
+console.log('Day of Birth', AgeInput)
+
