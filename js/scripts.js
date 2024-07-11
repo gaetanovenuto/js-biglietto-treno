5d1b1a8f51a302a1d8f8fda7850c2bbf48029802
gaetanovenuto <gaetanovenuto99@live.it>
2024-07-11 16:12:22 +0200

    Completato JS ed esercizio, manca da stamparlo sull'HTML, fixing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,43 @@
 function calculate() {
-    const KmInput = document.getElementById("KmInput");
-    ;
-    const KmFixedPrice = 0.21;
+    const KmAmount = document.getElementById("KmInput").value;
+    console.log("I km che l'utente vuole percorrere sono", KmAmount);
+
+    const DoBInput = document.getElementById("AgeInput").value;
+    console.log("La data di nascita dell'utente è", DoBInput);
+
+    const DateOfBirth = new Date(DoBInput);
+
+    const UserYear = DateOfBirth.getFullYear();
+    console.log("L'anno di nascita dell'utente è:", UserYear);
+
+    const KmPrice = 0.21;
+
+    const KmFinalPrice = KmAmount * KmPrice;
+    console.log("Il prezzo del biglietto senza sconti è di:", KmFinalPrice)
+
+    const now = new Date();
+    console.log("La data di oggi è:", now)
+
+    const CurrentYear = now.getFullYear();
+    console.log("L'anno corrente è:", CurrentYear);
+
+    const UserAge = CurrentYear - UserYear;
+    console.log("L'età dell'utente è di:", UserAge, "anni.");
+
+    if (UserAge < 18) {
+        let FinalPrice = (KmFinalPrice - (KmFinalPrice * 0.2));
+        console.log("Il prezzo del biglietto scontato sarà di: €" ,FinalPrice);
+    }
+
+    else if (UserAge > 65) {
+        let FinalPrice = (KmFinalPrice - (KmFinalPrice * 0.4));
+        console.log("Il prezzo del biglietto scontato sarà di: €" ,FinalPrice);
+    }
+    
+    else {
+        let FinalPrice = (KmFinalPrice * 1);
+        console.log("Il prezzo del biglietto pieno sarà di: €" ,FinalPrice);
+    }
+
+    document.getElementById('final-price').innerHTML = FinalPrice;
 }
-
-console.log("Hello World!")
-
-// INPUT AGE 
-const AgeInput = document.getElementBy("AgeInput");
-console.log('Day of Birth', AgeInput)
-
